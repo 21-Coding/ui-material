227179e385f1e5d2ab45e692cd4a5a4343f42241
Dom <dom@Doms-iMac.local>
2020-07-06 12:39:00 -0700

    return index.js to normal.

--- src/index.js
+++ src/index.js
@@ -4,17 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function App() {
-  return(
-
-  );
-}
-
 ReactDOM.render(
-  <App />
-document.querySelector('#app')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
